b3d105123d0ac0cc379e1e22d94e365e8f823a71
Olivier3456 <olivier.larrey@gmail.com>
2024-05-28 08:21:44 +0200

    Chapter 6 done

--- Assets/Resources/Script/PlayerShipBuild.cs
+++ Assets/Resources/Script/PlayerShipBuild.cs
@@ -17,6 +17,10 @@ public class PlayerShipBuild : MonoBehaviour
     private int bank = 600;
     private bool purchaseMade = false;
 
+    // For ads
+    private string placementId_rewardedVideo = "rewardedVideo";
+    string gameId = "1234567";
+
 
     private void Start()
     {
@@ -30,7 +34,26 @@ public class PlayerShipBuild : MonoBehaviour
         buyButton = textBoxPanel.transform.Find("BUY ?").gameObject;
         TurnOffPlayerShipVisuals();
         PreparePlayerShipForUpgrade();
+
+        // For ads
+        CheckPlatform();
     }
+
+
+    private void CheckPlatform()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            gameId = "PLACEHOLDER_IPHONE_GAME_ID";
+        }
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            gameId = "PLACEHOLDER_ANDROID_GAME_ID";
+        }
+
+        //Monetization.Initialize(gameId, false);
+    }
+
 
     private void TurnOffPlayerShipVisuals()
     {
@@ -110,9 +133,118 @@ public class PlayerShipBuild : MonoBehaviour
                         SoldOut();
                     }
                 }
+                else if (target.name == "BUY ?")
+                {
+                    BuyItem();
+                }
+                else if (target.name == "WATCH AD")
+                {
+                    WatchAdvert();
+                }
+                else if (target.name == "START")
+                {
+                    StartGame();
+                }
             }
         }
     }
+
+    private void WatchAdvert()
+    {
+        //if (Application.internetReachability != NetworkReachability.NotReachable)
+        {
+            ShowRewardedAds();
+        }
+    }
+
+    private void ShowRewardedAds()
+    {
+        StartCoroutine(WaitForAd());
+    }
+
+    private IEnumerator WaitForAd()
+    {
+        string placementId = placementId_rewardedVideo;
+        //while (!Monetization.IsReady(placementId))
+        {
+            yield return null;
+        }
+
+        //ShowAdPlacementContent ad = null;
+        //ad = Monetization.GetPlacementContent(placementId) as ShowAdPlacementContent;
+        //if (ad != null)
+        //{
+        //    ad.Show(AdFinished);
+        //}
+
+
+        // Calling the placeholder function instead of the real one:
+        AdFinished();
+    }
+
+    // Real function declaration:
+    // private void AdFinished(ShowResult result)
+    // Placeholder function declaration:
+    private void AdFinished()
+    {
+        //if (result == ShowResult.Finished)
+        {
+            bank += 300;
+            bankObj.GetComponentInChildren<TextMesh>().text = bank.ToString();
+            TurnOffSelectionHighlights();
+        }
+    }
+
+
+
+
+
+
+    private void BuyItem()
+    {
+        Debug.Log("PURCHASED");
+        purchaseMade = true;
+        buyButton.SetActive(false);
+        tmpSelection.SetActive(false);
+
+        for (int i = 0; i < visualWeapons.Length; i++)
+        {
+            if (visualWeapons[i].name == tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName)
+            {
+                visualWeapons[i].SetActive(true);
+            }
+        }
+
+        UpgradeToShip(tmpSelection.transform.parent.gameObject.GetComponent<ShopPiece>().ShopSelection.iconName);
+
+        bank -= Int32.Parse(tmpSelection.transform.parent.GetComponent<ShopPiece>().ShopSelection.cost);
+        bankObj.transform.Find("bankText").GetComponent<TextMesh>().text = bank.ToString();
+        tmpSelection.transform.parent.transform.Find("itemText").GetComponent<TextMesh>().text = "SOLD";
+    }
+
+    private void StartGame()
+    {
+        if (purchaseMade)
+        {
+            playerShip.name = "UpgradedShip";
+            if (playerShip.transform.Find("energy +1(Clone)"))
+            {
+                playerShip.GetComponent<Player>().Health = 2;
+                Debug.Log("Player has a health of 2");
+            }
+            DontDestroyOnLoad(playerShip);
+        }
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("testLevel");
+    }
+
+    private void UpgradeToShip(string upgrade)
+    {
+        GameObject shipItem = GameObject.Instantiate(Resources.Load($"Prefab/Player/{upgrade}")) as GameObject;
+        shipItem.transform.SetParent(playerShip.transform);
+        shipItem.transform.localPosition = Vector3.zero;
+    }
+
 
     private void Affordable()
     {
--- Assets/Resources/Script/PlayerSpawner.cs
+++ Assets/Resources/Script/PlayerSpawner.cs
@@ -1,27 +1,45 @@
 ﻿using UnityEngine;
 
-public class PlayerSpawner : MonoBehaviour 
+public class PlayerSpawner : MonoBehaviour
 {
-	SOActorModel actorModel;
-	GameObject playerShip;
-	
-	void Start()
-	{
-		CreatePlayer();
-	}
-	
-	void CreatePlayer()
-	{
-		//CREATE PLAYER
-        actorModel = Object.Instantiate(Resources.Load("Script/ScriptableObject/Player_Default")) as SOActorModel;
-        playerShip = GameObject.Instantiate(actorModel.actor) as GameObject;
-        playerShip.GetComponent<Player>().ActorStats(actorModel);
+    SOActorModel actorModel;
+    GameObject playerShip;
 
-		//SET PLAYER UP
-		playerShip.transform.rotation = Quaternion.Euler(0,180,0);
-		playerShip.transform.localScale = new Vector3(60,60,60);
-		playerShip.GetComponentInChildren<ParticleSystem>().transform.localScale = new Vector3(25,25,25);
-		playerShip.name = "Player";
-		playerShip.transform.SetParent(this.transform); 
-	}
+    private bool upgradedShip = false;
+
+    void Start()
+    {
+        CreatePlayer();
+
+        GetComponentInChildren<Player>().enabled = true;
+    }
+
+    void CreatePlayer()
+    {
+        if (GameObject.Find("UpgradedShip"))
+        {
+            upgradedShip = true;
+        }
+
+        if (!upgradedShip || GameManager.Instance.Died)
+        {
+            GameManager.Instance.Died = false;
+            actorModel = Object.Instantiate(Resources.Load("Script/ScriptableObject/Player_Default")) as SOActorModel;
+            playerShip = GameObject.Instantiate(actorModel.actor, this.transform.position, Quaternion.Euler(270, 180, 0));
+            playerShip.GetComponent<IActorTemplate>().ActorStats(actorModel);
+        }
+        else
+        {
+            playerShip = GameObject.Find("UpgradedShip");
+        }
+
+
+        //SET PLAYER UP
+        playerShip.transform.rotation = Quaternion.Euler(0, 180, 0);
+        playerShip.transform.localScale = new Vector3(60, 60, 60);
+        playerShip.GetComponentInChildren<ParticleSystem>().transform.localScale = new Vector3(25, 25, 25);
+        playerShip.name = "Player";
+        playerShip.transform.SetParent(this.transform);
+        playerShip.transform.position = Vector3.zero;
+    }
 }
